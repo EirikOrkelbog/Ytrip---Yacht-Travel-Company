94cc8bb2c5c3a7914dcb809fe9969be375bcbdd0
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-12 21:11:51 +0200

    added play and pause functions

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,5 +1,7 @@
 import header from "./modules/header.js";
 import trips from "./modules/trips.js";
+import video from "./modules/video.js";
 
 header();
 trips();
+video();
---
+++ _app/js/modules/video.js
@@ -0,0 +1,29 @@
+export default function video() {
+	const button = document.querySelector(".video__button");
+	const video = document.querySelector(".video__player video");
+
+	button.addEventListener('click', handleButtonClick);
+	video.addEventListener('click', handleVideoClick);
+
+	function handleButtonClick() {
+		playVideo();
+	}
+
+	function handleVideoClick() {
+		pauseVideo();
+	}
+
+	function playVideo() {
+		if(video.paused) {
+			video.play();
+			button.classList.add('video__hide');
+		}
+	}
+
+	function pauseVideo() {
+		if(video.play) {
+			video.pause();
+			button.classList.remove('video__hide');
+		}
+	}
+}
