9302492c6c82e0ef9c9a998619abc1c7bd4e60bb
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-22 10:52:43 +0200

    imported js files from reviews and faq

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,7 +1,11 @@
 import header from "./modules/header.js";
 import trips from "./modules/trips.js";
 import video from "./modules/video.js";
+import reviews from "./modules/reviews.js";
+import faq from "./modules/faq.js";
 
 header();
 trips();
 video();
+reviews();
+faq();
