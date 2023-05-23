842ddd676f65e3d36a087d56e1ebcdfd7b3bf9a7
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-23 22:47:05 +0200

    added navigation schema

---
+++ _studio/schemas/documents/navigation.js
@@ -0,0 +1,27 @@
+export default {
+	name: 'navigation',
+	title: 'Navigation',
+	type: 'document',
+	fields: [
+		{
+			name: 'logo',
+			title: 'Logo',
+			type: 'image',
+		},
+		{
+			name: 'title',
+			title: 'Title',
+			type: 'string',
+		},
+		{
+			name: 'link',
+			title: 'Link',
+			type: 'url',
+		},
+		{
+			name: 'menuIcon',
+			title: 'Menu Icon',
+			type: 'image',
+		}
+	],
+};
