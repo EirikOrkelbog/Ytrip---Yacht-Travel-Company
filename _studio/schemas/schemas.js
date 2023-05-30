eb9b17e5ed6672be12ef2de53736ac44c52223ee
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-30 10:58:49 +0200

    imported schemas

--- _studio/schemas/schemas.js
+++ _studio/schemas/schemas.js
@@ -2,10 +2,18 @@ import navigation from "./documents/navigation.js"
 import blog from "./documents/blog.js"
 import writer from "./documents/writer.js"
 import cruises from "./documents/cruises.js"
+import reviews from "./documents/reviews.js"
+import faq from "./documents/faq.js"
+import faqObject from "./documents/faq-object.js"
+import settings from "./documents/settings.js"
 
 export default [
 	navigation,
 	blog,
 	writer,
 	cruises,
+	reviews,
+	faq,
+	faqObject,
+	settings,
 ]
