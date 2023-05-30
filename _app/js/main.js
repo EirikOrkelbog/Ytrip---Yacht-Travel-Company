b7e2cbb66161ee1527ff77ca0abd6ad5834d01c4
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-30 10:56:03 +0200

    imported functions to main

--- _app/js/main.js
+++ _app/js/main.js
@@ -2,10 +2,14 @@ import header from "./modules/header.js";
 import trips from "./modules/trips.js";
 import video from "./modules/video.js";
 import reviews from "./modules/reviews.js";
-import faq from "./modules/faq.js";
+import blogSection from "./modules/blog-section.js";
+import faqFunctionality from "./modules/faq-functionality.js";
+import faqSection from "./modules/faq-section.js";
 
 header();
 trips();
 video();
 reviews();
-faq();
+blogSection();
+faqFunctionality();
+faqSection();
