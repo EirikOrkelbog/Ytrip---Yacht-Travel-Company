fdd8c99a1534099687a27d535afc6c0ac16c6324
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-30 10:38:44 +0200

    organized sanity schemas

---
+++ _studio/schemas/structure/ytrip.js
@@ -0,0 +1,23 @@
+export default Structure => {
+	const { divider, editor, list, listItem, documentTypeListItem } = Structure;
+
+	return list()
+		.title('Ytrip content')
+		.showIcons(false)
+		.items([
+			documentTypeListItem('navigation'),
+			documentTypeListItem('cruises'),
+			documentTypeListItem('blogPost'),
+			documentTypeListItem('writer'),
+			documentTypeListItem('reviews'),
+			documentTypeListItem('faq'),
+
+			divider(),
+
+			listItem()
+				.title('Settings')
+				.child(
+					editor().id('settings').schemaType('settings')
+				),
+		])
+}
