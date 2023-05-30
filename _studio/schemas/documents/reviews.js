5828abaaf89dde389cd53e529135f7d876ac9a13
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-30 10:40:33 +0200

    added review schema

---
+++ _studio/schemas/documents/reviews.js
@@ -0,0 +1,32 @@
+export default {
+	name: 'reviews',
+	title: 'Reviews',
+	type: 'document',
+	fields: [
+		{
+			name: 'reviewTitle',
+			title: 'Review Title',
+			type: 'string',
+		},
+		{
+			name: 'review',
+			title: 'Review',
+			type: 'text',
+		},
+		{
+			name: 'reviewer',
+			title: 'Reviewer Name',
+			type: 'string',
+		},
+		{
+			name: 'destination',
+			title: 'Destination',
+			type: 'string',
+		},
+		{
+			name: 'tripImage',
+			title: 'Image from Trip',
+			type: 'image',
+		},
+	],
+};
