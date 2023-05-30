01143e927c605ad6467c74a7fff9f027b2dae630
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-30 10:45:01 +0200

    formatted date on cruises

--- _app/js/cruises-page.js
+++ _app/js/cruises-page.js
@@ -1,91 +1,112 @@
 import { sanity } from "./sanity.js";
+import header from "./modules/header.js";
+header();
 
 export default async function cruisesPage() {
-	fetch('https://5khmsjz8.api.sanity.io/v2021-10-21/data/query/production?query=*[_type == "cruises"]')
-		.then(response => response.json())
-		.then(data => {
-			const cruises = data.result;
-			const container = document.querySelector('.cruises');
+	const query = `*[_type == 'cruises']`;
+	const cruises = await sanity.fetch(query);
 
-			async function renderCruisesHTML() {
-				for (const cruise of cruises) {
+	const container = document.querySelector('.cruises');
 
-					const imageAssetId = cruise.image.asset._ref;
-					const imageAssetQuery = `*[_id == "${imageAssetId}"]`;
-					const imageAsset = await sanity.fetch(imageAssetQuery);
-					const imageUrl = imageAsset[0].url;
+	async function renderCruisesHTML() {
+		for (const cruise of cruises) {
 
-					const cruiseHolder = document.createElement('div');
-					const cruiseImageContainer = document.createElement('div');
-					const cruiseImage = document.createElement('img');
-					const cruisesButton = document.createElement('a');
-					const cruiseContainer = document.createElement('div');
-					const cruiseTitle = document.createElement('h2');
-					const cruiseDetailsDuration = document.createElement('div');
-					const cruiseDuration = document.createElement('p');
-					const cruiseDays = document.createElement('span');
-					const cruiseDetailsDate = document.createElement('div');
-					const cruiseDate = document.createElement('p');
-					const cruiseDateTime = document.createElement('span');
-					const cruiseDetailsPrice = document.createElement('div');
-					const cruisePrice = document.createElement('p');
-					const cruisePriceNumber = document.createElement('span');
-					const cruiseIncluded = document.createElement('p');
-					const cruiseIncludedContainer = document.createElement('ul');
+			const imageAssetId = cruise.image.asset._ref;
+			const imageAssetQuery = `*[_id == "${imageAssetId}"]`;
+			const imageAsset = await sanity.fetch(imageAssetQuery);
+			const imageUrl = imageAsset[0].url;
 
-					cruiseHolder.className = 'cruise';
-					cruiseImageContainer.className = 'cruises__container';
-					cruiseContainer.className = 'cruises__container';
-					cruiseImage.src = imageUrl;
-					cruiseImage.alt = '';
-					cruiseImage.className = 'cruises img';
-					cruisesButton.className = 'button cruises__button'
-					cruisesButton.href = '/pages/articles.html';
-					cruisesButton.textContent = 'Book trip';
-					cruiseTitle.textContent = cruise.title;
-					cruiseTitle.className = 'subheading-mobile';
-					cruiseIncludedContainer.className = 'cruises__included';
-					cruiseDetailsDuration.className = 'cruises__details';
-					cruiseDays.className = 'cruises p';
-					cruiseDays.textContent = `${cruise.duration} days`;
-					cruiseDuration.textContent = 'Duration:';
-					cruiseDate.className = 'cruises p';
-					cruiseDate.textContent = 'Date:';
-					cruiseDateTime.textContent = `${cruise.dateStart} - ${cruise.dateEnd}`;
-					cruiseDetailsPrice.className = 'cruises__details';
-					cruiseDetailsDate.className = 'cruises__details';
-					cruisePrice.className = 'cruises p';
-					cruisePrice.textContent = 'Price:';
-					cruisePriceNumber.textContent = `$${cruise.price}`;
-					cruiseIncluded.className = 'cruises p';
-					cruiseIncluded.textContent = 'Included in price:';
+			// Formatting dateStart from yyyy-mm-dd to dd.mm.yyyy
+			const dateStart = cruise.dateStart;
+			const start = new Date(dateStart);
+			const startYear = start.getFullYear();
+			let startMonth = start.getMonth() + 1;
+			let startDay = start.getDate();
+			if (startDay < 10) startDay = '0' + startDay;
+			if (startMonth < 10) startMonth = '0' + startMonth;
+			const formattedDateStart = startDay + '.' + startMonth + '.' + startYear;
 
-					cruiseImageContainer.appendChild(cruiseImage);
-					cruiseImageContainer.appendChild(cruisesButton);
-					cruiseContainer.appendChild(cruiseTitle);
-					cruiseContainer.appendChild(cruiseDetailsDuration);
-					cruiseContainer.appendChild(cruiseDetailsDate);
-					cruiseContainer.appendChild(cruiseDetailsPrice);
-					cruiseContainer.appendChild(cruiseIncluded);
-					cruiseContainer.appendChild(cruiseIncludedContainer);
-					cruiseDetailsDate.appendChild(cruiseDate);
-					cruiseDetailsDate.appendChild(cruiseDateTime);
-					cruiseDetailsDuration.appendChild(cruiseDuration);
-					cruiseDetailsDuration.appendChild(cruiseDays);
-					cruiseDetailsPrice.appendChild(cruisePrice);
-					cruiseDetailsPrice.appendChild(cruisePriceNumber);
-					cruiseHolder.appendChild(cruiseImageContainer);
-					cruiseHolder.appendChild(cruiseContainer);
-					container.appendChild(cruiseHolder);
+			// Formatting dateEnd
+			const dateEnd = cruise.dateEnd;
+			const end = new Date(dateEnd);
+			const endYear = end.getFullYear();
+			let endMonth = end.getMonth();
+			let endDay = end.getDate(); 
+			if (endDay < 10) endDay = '0' + endDay;
+			if (endMonth < 10) endMonth = '0' + endMonth;
+			const formattedDateEnd = endDay + '.' + endMonth + '.' + endYear;
 
-					for (let i = 0; i < cruise.included.length; i++) {
-						const liElement = document.createElement('li');
-						liElement.textContent = cruise.included[i];
-						cruiseIncludedContainer.appendChild(liElement);
-					}
-				}
+			const cruiseHolder = document.createElement('div');
+			const cruiseImageContainer = document.createElement('div');
+			const cruiseImage = document.createElement('img');
+			const cruisesButton = document.createElement('a');
+			const cruiseContainer = document.createElement('div');
+			const cruiseTitle = document.createElement('h2');
+			const cruiseDetailsDuration = document.createElement('div');
+			const cruiseDuration = document.createElement('p');
+			const cruiseDays = document.createElement('span');
+			const cruiseDetailsDate = document.createElement('div');
+			const cruiseDate = document.createElement('p');
+			const cruiseDateTime = document.createElement('span');
+			const cruiseDetailsPrice = document.createElement('div');
+			const cruisePrice = document.createElement('p');
+			const cruisePriceNumber = document.createElement('span');
+			const cruiseIncluded = document.createElement('p');
+			const cruiseIncludedContainer = document.createElement('ul');
+
+			cruiseHolder.className = 'cruise';
+			cruiseImageContainer.className = 'cruises__container';
+			cruiseContainer.className = 'cruises__container';
+			cruiseImage.src = imageUrl;
+			cruiseImage.alt = '';
+			cruiseImage.className = 'cruises img';
+			cruisesButton.className = 'button cruises__button'
+			cruisesButton.href = '/pages/articles.html';
+			cruisesButton.textContent = 'Book trip';
+			cruiseTitle.textContent = cruise.title;
+			cruiseTitle.className = 'subheading-mobile';
+			cruiseIncludedContainer.className = 'cruises__included';
+			cruiseDetailsDuration.className = 'cruises__details';
+			cruiseDays.className = 'cruises p';
+			cruiseDays.textContent = `${cruise.duration} days`;
+			cruiseDuration.textContent = 'Duration:';
+			cruiseDate.className = 'cruises p';
+			cruiseDate.textContent = 'Date:';
+			cruiseDateTime.textContent = `${formattedDateStart} - ${formattedDateEnd}`;
+			cruiseDetailsPrice.className = 'cruises__details';
+			cruiseDetailsDate.className = 'cruises__details';
+			cruisePrice.className = 'cruises p';
+			cruisePrice.textContent = 'Price:';
+			cruisePriceNumber.textContent = `$${cruise.price}`;
+			cruiseIncluded.className = 'cruises p';
+			cruiseIncluded.textContent = 'Included in price:';
+
+			cruiseImageContainer.appendChild(cruiseImage);
+			cruiseImageContainer.appendChild(cruisesButton);
+			cruiseContainer.appendChild(cruiseTitle);
+			cruiseContainer.appendChild(cruiseDetailsDuration);
+			cruiseContainer.appendChild(cruiseDetailsDate);
+			cruiseContainer.appendChild(cruiseDetailsPrice);
+			cruiseContainer.appendChild(cruiseIncluded);
+			cruiseContainer.appendChild(cruiseIncludedContainer);
+			cruiseDetailsDate.appendChild(cruiseDate);
+			cruiseDetailsDate.appendChild(cruiseDateTime);
+			cruiseDetailsDuration.appendChild(cruiseDuration);
+			cruiseDetailsDuration.appendChild(cruiseDays);
+			cruiseDetailsPrice.appendChild(cruisePrice);
+			cruiseDetailsPrice.appendChild(cruisePriceNumber);
+			cruiseHolder.appendChild(cruiseImageContainer);
+			cruiseHolder.appendChild(cruiseContainer);
+			container.appendChild(cruiseHolder);
+
+			for (let index = 0; index < cruise.included.length; index++) {
+				const liElement = document.createElement('li');
+				liElement.textContent = cruise.included[index];
+				cruiseIncludedContainer.appendChild(liElement);
 			}
-			renderCruisesHTML();
-		});
+		}
+	}
+	renderCruisesHTML();
+
 }
 cruisesPage();
