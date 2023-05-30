1be9cdee75216a0b48be96afc08290472ec19dac
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-30 10:58:22 +0200

    added js to hide navigation

--- _app/js/modules/header.js
+++ _app/js/modules/header.js
@@ -1,14 +1,29 @@
 export default function header() {
 	const menu = document.querySelector('.menu');
 	const navLinks = document.querySelector('.nav__links');
+	const links = document.querySelectorAll('.nav__links li');
 
 	menu.addEventListener('click', handleMenuClick);
 
-	function handleMenuClick() {
-		toggleMenu();
+	links.forEach(link => {
+		link.addEventListener('click', handleLinkClick);
+	});
+
+	window.addEventListener('click', function(event) {
+		if (!navLinks.contains(event.target) && !menu.contains(event.target)) {
+			navLinks.classList.remove('active');
+		}
+	});
+
+	function handleLinkClick(event) {
+		navLinks.classList.remove('active');
 	}
-	
-	function toggleMenu(event) {
+
+	function handleMenuClick(event) {
+		toggleMenu(event);
+	}
+
+	function toggleMenu() {
 		navLinks.classList.toggle('active');
 	}
 }
