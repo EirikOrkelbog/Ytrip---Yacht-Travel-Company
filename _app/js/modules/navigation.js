c7dfd23b82f46871b54c2fe48bb702bae05807d9
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-05-31 16:20:20 +0200

    added navigation with js

--- _app/js/modules/navigation.js
+++ _app/js/modules/navigation.js
@@ -1,29 +1,41 @@
-export default function navigation() {
-	const menu = document.querySelector('.menu');
-	const navLinks = document.querySelector('.nav__links');
-	const links = document.querySelectorAll('.nav__links li');
+import { sanity } from "../sanity.js";
+import navigation from "./navigation-functionality.js";
 
-	menu.addEventListener('click', handleMenuClick);
-
-	links.forEach(link => {
-		link.addEventListener('click', handleLinkClick);
-	});
-
-	window.addEventListener('click', function(event) {
-		if (!navLinks.contains(event.target) && !menu.contains(event.target)) {
-			navLinks.classList.remove('active');
+export default async function navigationDOM() {
+	const query = `*[_type == 'navigation']`;
+	const items = await sanity.fetch(query);
+	
+	const container = document.querySelector('.nav__header');
+	
+	async function renderNavigationDOM() {
+		const nav = document.createElement('nav');
+		const logo = document.createElement('a');
+		const menu = document.createElement('button');
+		const navLinks = document.createElement('ul');
+		
+		nav.className = 'nav nav__underline';
+		logo.className = 'header__logo';
+		logo.setAttribute('href', '/index.html');
+		menu.className = 'menu';
+		navLinks.className = 'nav__links nav__links-black';
+		
+		container.appendChild(nav);
+		nav.appendChild(logo);
+		nav.appendChild(menu);
+		nav.appendChild(navLinks);
+		
+		const linkNames = items[0].links.linkName;
+		const linkHrefs = items[0].links.linkHref;
+		
+		for (let i = 0; i < linkNames.length; i++) {
+			const link = document.createElement('li');
+			const linkAnchor = document.createElement('a');
+			linkAnchor.textContent = linkNames[i];
+			linkAnchor.href = linkHrefs[i];
+			link.appendChild(linkAnchor);
+			navLinks.appendChild(link);
 		}
-	});
-
-	function handleLinkClick(event) {
-		navLinks.classList.remove('active');
 	}
-
-	function handleMenuClick(event) {
-		toggleMenu(event);
-	}
-
-	function toggleMenu() {
-		navLinks.classList.toggle('active');
-	}
+	renderNavigationDOM();
+	navigation();
 }
