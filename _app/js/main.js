3356b417a853ae0e44736d0440793b04e4fb5730
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-06-01 15:36:21 +0200

    removed functions from main.js

--- _app/js/main.js
+++ _app/js/main.js
@@ -1,19 +1,15 @@
-import navigationDOM from "./modules/navigation.js";
+import navigationDOM from "./modules/navigationDOM.js";
 import settingsDOM from "./modules/settingsDOM.js";
-import trips from "./modules/trips.js";
-import video from "./modules/video.js";
-import reviews from "./modules/reviews.js";
+import tripsDOM from "./modules/tripsDOM.js";
+import reviewsDOM from "./modules/reviewsDOM.js";
 import blogSection from "./modules/blog-section.js";
-// import faqFunctionality from "./modules/faq-functionality.js";
-import faqSection from "./modules/faq-section.js";
+import faqDOM from "./modules/faqDOM.js";
 import footerDOM from "./modules/footer.js";
 
 navigationDOM();
 settingsDOM();
-trips();
-video();
-reviews();
+tripsDOM();
+reviewsDOM();
 blogSection();
-// faqFunctionality();
-faqSection();
+faqDOM();
 footerDOM();
