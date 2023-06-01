10ff8425d61b457a951efadba0f89f1ef26b0821
Eirik Orkelbog <111751379+EirikOrkelbog@users.noreply.github.com>
2023-06-01 15:38:16 +0200

    updated faq

---
+++ _app/js/modules/faq.js
@@ -0,0 +1,38 @@
+export default function faq() {
+	const faqItems = document.querySelectorAll('.faq__item');
+	const icons = document.querySelectorAll('.faq__item img');
+
+	faqItems.forEach((item) => {
+		const answer = item.querySelector('.faq__answer');
+		const icon = item.querySelector('img');
+
+		item.addEventListener('click', () => {
+			answer.classList.toggle('hide-answer');
+
+			if (answer.className.includes('hide-answer')) {
+				icon.src = '/_app/assets/icons/plus.svg';
+			} else {
+				icon.src = '/_app/assets/icons/minus.svg';
+			}
+
+			faqItems.forEach((otherItem) => {
+				if (otherItem !== item) {
+					const otherAnswer = otherItem.querySelector('.faq__answer');
+					const otherIcon = otherItem.querySelector('img');
+
+					otherAnswer.classList.add('hide-answer');
+					otherIcon.src = '/_app/assets/icons/plus.svg';
+				}
+			});
+		});
+
+		icons.forEach(otherIcon => {
+			if (otherIcon !== icon) {
+				otherIcon.src = '/_app/assets/icons/plus.svg';
+			}
+		});
+
+		answer.classList.add('hide-answer');
+		icon.src = '/_app/assets/icons/plus.svg';
+	});
+}
---
+++ _app/js/modules/faqDOM.js
@@ -0,0 +1,54 @@
+import { sanity } from "../sanity.js";
+import faq from "./faq.js";
+
+export default async function faqDOM() {
+	const query = `*[_type == 'faq'][0]`;
+	const faqs = await sanity.fetch(query);
+
+	const container = document.querySelector('.faq');
+
+	function renderFaqHTML() {
+		const faqContainer = document.createElement('div');
+		const faqContentContainer = document.createElement('div');
+		const faqSubheading = document.createElement('h3');
+		const faqHeading = document.createElement('h2');
+		const faqImage = document.createElement('img');
+
+		faqContainer.className = 'faq__container';
+		faqContentContainer.className = 'faq__container faq__questions';
+		faqSubheading.textContent = 'FAQ';
+		faqSubheading.className = 'faq__subheading';
+		faqHeading.className = 'subheading-mobile';
+		faqHeading.textContent = 'Frequently asked questions';
+		faqImage.src = '/_app/assets/images/compressed//faq.jpg';
+		faqImage.className = 'faq__image';
+
+		faqContainer.appendChild(faqSubheading);
+		faqContainer.appendChild(faqHeading);
+		faqContainer.appendChild(faqImage);
+		container.appendChild(faqContainer);
+		container.appendChild(faqContentContainer);
+
+		for (let i = 0; i < faqs.faqs.length; i++) {
+			const faqItem = document.createElement('div');
+			const faqQuestionContainer = document.createElement('div');
+			const faqIcon = document.createElement('img');
+			const faqQuestion = document.createElement('p');
+			const faqAnswer = document.createElement('p');
+
+			faqItem.className = 'faq__item';
+			faqQuestion.className = 'faq__question';
+			faqQuestion.textContent = faqs.faqs[i].question;
+			faqAnswer.className = 'faq__answer hide-answer';
+			faqAnswer.textContent = faqs.faqs[i].answer;
+
+			faqQuestionContainer.appendChild(faqIcon);
+			faqQuestionContainer.appendChild(faqQuestion);
+			faqItem.appendChild(faqQuestionContainer);
+			faqItem.appendChild(faqAnswer);
+			faqContentContainer.appendChild(faqItem);
+		}
+	}
+	renderFaqHTML();
+	faq();
+}
